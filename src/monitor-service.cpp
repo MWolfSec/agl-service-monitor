d6d4d158a944ea518877227f2fe5697b48fef71d
MWolfAVL <125113761+MWolfSec@users.noreply.github.com>
2023-02-17 09:02:05 +0100

    fixed monitor-service.cpp
    fixed monitor-service.cpp

--- src/monitor-service.cpp
+++ src/monitor-service.cpp
@@ -7,8 +7,7 @@
 
 MonitorService::MonitorService(const VisConfig &config, net::io_context& ioc, ssl::context& ctx) :
 	VisSession(config, ioc, ctx),
-	m_can_helper(),
-	m_led_helper()
+	m_can_helper()
 {
 }
 
@@ -27,18 +26,18 @@ void MonitorService::handle_notification(std::string &path, std::string &value, 
 {
 	if (path == "Vehicle.Speed") {
 		try {
-			int temp = std::stoi(value);
-			if (temp >= 0 && temp < 256)
-				set_right_temperature(temp);
+			int speed = std::stoi(value);
+			if (speed >= 0 && speed < 180)
+				set_speed(speed);
 		}
 		catch (std::exception ex) {
 			// ignore bad value
 		}
 	} else if (path == "Vehicle.Powertrain.CombustionEngine.Speed") {
 		try {
-			int speed = std::stoi(value);
-			if (speed >= 0 && speed < 256)
-				set_fan_speed(speed);
+			int rpm = std::stoi(value);
+			if (rpm >= 0 && rpm < 5000)
+				set_rpm(rpm);
 		}
 		catch (std::exception ex) {
 			// ignore bad value
